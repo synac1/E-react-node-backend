c0d862f1e0d435f9f6859c97decffcd3ccc11c03
ram <ramvilla997@gmail.com>
2023-08-30 22:46:00 -0400

    cannot GET error

--- server.js
+++ server.js
@@ -6,28 +6,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 const db = require("./db"); 
 db.sequelize.authenticate()
   .then(() => {
     console.log("Database connection has been established successfully.");
-    app.get("/", (req, res) => {
-      res.send("Welcome to your server!");
-    });
-  
   })
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
   });
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Move the root route handler outside the database connection block
+app.get("/", (req, res) => {
+  res.send("Welcome to your server!");
+});
 
-app.use("/api/users",userRoutes); // Mount user routes
+app.use("/api/users", userRoutes); // Mount user routes
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
