1501d90b8afdecdd2f3968302f71afc3e103b881
ottawa-ehospital <uottawabiomedicalsystems@gmail.com>
2023-10-17 01:11:55 -0400

    search backend

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+var mysql = require("./dbConnection");
 const db = require("./db"); 
 db.sequelize.authenticate()
   .then(() => {
@@ -28,6 +29,57 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes); // Mount user routes
+
+app.post("/searchpatient", (req, res) => {
+  
+  const phoneNumber = req.body.phoneNumber; // patient phone number, e.g. "6131230000"
+  console.log(phoneNumber);
+  // Check patient identity
+  if (!phoneNumber) {
+      res.send({ error: "Missing patient phone number" });
+      return;
+  }
+  var patient_id = 0;
+  var check_list = [];
+  let sqlDB = mysql.connect();
+  sql = `
+      SELECT *
+      FROM patients_registration 
+      WHERE MobileNumber = "${phoneNumber}"
+  `;
+  console.log(sql);
+  sqlDB.query(sql, (error, result) => {
+      if (error) {
+          res.send({ error: "Something wrong in MySQL." });
+          console.log("Something wrong in MySQL");
+          return;
+      }
+      if (result.length != 1) {
+          check_list[0] = 1;
+          // res.render('pages/searchpatient', {check:check_list});
+          res.send({ error: "No patient matched in database." });
+          
+          return;
+      }
+      patient_id = result[0].id;
+      console.log("Pid:",patient_id);
+      sql_search_query = `SELECT * FROM patients_registration WHERE id = "${patient_id}"`;
+      let sqlDB = mysql.connect();
+      sqlDB.query(sql_search_query, function (err, result) {
+          if (err) throw err;
+
+          ///res.render() function
+         // res.send(result.id);
+          res.json(result[0]);
+          console.log(result[0]);
+      });
+      sqlDB.end();
+      
+      //console.log(sql_search_query);
+  });
+  sqlDB.end();
+});
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
